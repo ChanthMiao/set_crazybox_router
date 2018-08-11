2f4d0a88a7615b01e6aeebaf4d55dfa9b0b4bcaa
shieldemail <39488343+shieldemail@users.noreply.github.com>
2018-08-11 22:45:17 +0800

    Add files via upload
    works well at python3

---
+++ setting.py
@@ -0,0 +1,44 @@
+# coding=utf-8
+# 添加引用
+import requests
+import pickle
+# 尝试导入先前账号密码
+try:
+    ins = open('data.pkl', 'rb')
+    payload2 = pickle.load(ins)
+except Exception as err:
+    # 否则输入宽带账号密码
+    name = input(u"输入账号")
+    password = input(u"输入密码")
+    # 设置更改宽带设置所需字典
+    payload2 = {
+        "cbid.network.wan.username": name,
+        "cbid.network.wan.password": password,
+        "cbid.network.wan.randommac": "1",
+        "cbid.network.wan.autoredial": "1",
+        "cbid.network.wan.dialtype": "3",
+        "cbid.network.wan.proto": "pppoe",
+        "cbid.network.wan.macaddr": "",
+        "step": "4",
+        "quit": "0",
+    }
+
+# 新建一个requests
+session_requests = requests.session()
+# 指定访问url
+login_url = 'http://192.168.1.1/cgi-bin/luci'
+# 设置登录所需字典
+payload = {"luci_username": "root", "luci_password": "admin"}
+# POST操作登录
+result = session_requests.post(login_url, data=payload)
+# 生成新的跳转url
+new_url = result.url + "/admin/guide"
+# POST操作修改宽带设置
+result2 = session_requests.post(new_url, data=payload2)
+
+# 打印结果提示页面
+print(result2.content)
+# 存储账号密码
+output = open('data.pkl', 'wb')
+pickle.dump(payload2, output)
+output.close()
